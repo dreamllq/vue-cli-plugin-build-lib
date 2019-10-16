1e6df5739d833be6ac4b4d7cc030e171e7f08c7c
lvliqi <lvliqi@360.net>
2019-10-16 16:23:49 +0800

    update

--- generator.js
+++ generator.js
@@ -1,10 +1,15 @@
 module.exports = (api, options, rootOptions) => {
     // 修改 `package.json` 里的字段
     api.extendPackage({
-      scripts: {
-        "build": "vue-cli-service build --target lib --name lib --formats umd,umd-min  src/index.js --report"
-      }
+        scripts: {
+            "build": "vue-cli-service build --target lib --name lib --formats umd,umd-min  src/index.js --report"
+        },
+        "main": "dist/lib.umd.min.js",
+        "files": [
+            "dist/*",
+            "src/index.vue"
+        ],
     })
 
     api.render('./template')
-  }
+}
