a32fb0dd70cf9a1bf2f81faeb0bdbce45fbfac55
吕立奇 <lvliqi@qianxin.com>
2019-10-30 17:57:15 +0800

    添加指令

--- generator/index.js
+++ generator/index.js
@@ -1,24 +1,25 @@
 module.exports = (api, options, rootOptions) => {
-    // 修改 `package.json` 里的字段
-    api.extendPackage({
-        scripts: {
-            "build": "vue-cli-service build --target lib --name lib --formats umd,umd-min  src/index.js --report"
-        },
-        "main": "dist/lib.umd.min.js",
-        "files": [
-            "dist/*",
-            "src/index.vue"
-        ],
-    })
+  // 修改 `package.json` 里的字段
+  api.extendPackage({
+    scripts: {
+      "build": "vue-cli-service build --target lib --name lib --formats umd,umd-min  src/index.js --report",
+      "postversion": "npm run build && git push && git push --tags && npm publish"
+    },
+    "main": "dist/lib.umd.min.js",
+    "files": [
+      "dist/*",
+      "src/index.vue"
+    ],
+  })
 
-    api._injectFileMiddleware(function (files) {
-        delete files['public/favicon.ico'];
-        delete files['public/index.html'];
-        delete files['src/assets/logo.png'];
-        delete files['src/components/HelloWorld.vue'];
-        delete files['src/App.vue'];
-        delete files['src/main.js'];
-    });
+  api._injectFileMiddleware(function (files) {
+    delete files['public/favicon.ico'];
+    delete files['public/index.html'];
+    delete files['src/assets/logo.png'];
+    delete files['src/components/HelloWorld.vue'];
+    delete files['src/App.vue'];
+    delete files['src/main.js'];
+  });
 
-    api.render('./template')
+  api.render('./template')
 }
